9b9853c0afb729d4757dbd4662a87a8f0ed51a8c
Pablo Trinidad <pabloatp0204@gmail.com>
2015-03-20 19:52:58 -0700

    Google Code Jam 2014 - Magic Trick with minified version

--- CodeJam2014-MagicTrick/MagicTrickMinified.py
+++ CodeJam2014-MagicTrick/MagicTrickMinified.py
@@ -2,35 +2,25 @@ input_file = open('A-small-practice.in')
 output_file = open('output.txt', 'w')
 lines = input_file.readlines()
 rows_positions = range(1, len(lines), 5)
-row = 1
 first_selected_row = 0
 last_selected_row = 0
 case = 1
+row = 1
 second_choose = False
-header_color = '\033[94m'
-end_color = '\033[0m'
 for i in range(1,len(lines)):
 	if i in rows_positions[::2]:
 		first_selected_row = int(lines[i][:-1])
 		row = 1
 		second_choose = False
-		print '_'*80
-		print header_color, '\t\t\tCASE', case, end_color
-		print header_color,'FIRST SELECTED ROW =',end_color, first_selected_row, header_color,'/ LINE =',end_color, i+1
-
 	if i in rows_positions[1::2]:
 		last_selected_row = int(lines[i][:-1])
 		row = 1
 		second_choose = True
-		print header_color,'LAST SELECTED ROW =',end_color, last_selected_row, header_color,'/ LINE =',end_color, i+1
-
 	if i not in rows_positions:
 		if not second_choose and first_selected_row == row:
 			first_selected_row_values = [int(x) for x in lines[i][:-1].split(' ')]
-			print header_color,'FIRST SELECTED ROW VALUES =',end_color, first_selected_row_values, header_color,'/ LINE =',end_color, i+1
 		elif second_choose and last_selected_row == row:
 			last_selected_row_values = [int(x) for x in lines[i][:-1].split(' ')]
-			print header_color,'LAST SELECTED ROW VALUES =',end_color, last_selected_row_values, header_color,'/ LINE =',end_color, i+1
 		if second_choose and row == 4:
 			repeated_times = 0
 			value = ''
